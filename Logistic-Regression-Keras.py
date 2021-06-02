1f792979ce09c275e4a92c64df0cb1d7f2300485
fasthill <kangeun@naver.com>
2021-06-02 23:29:57 +0900

    lecture add

---
+++ Logistic-Regression-Keras.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.optimizers import SGD, Adam
+
+import numpy as np
+
+# print(tf.__version__)
+
+import os
+
+try :
+    loaded_data = np.loadtxt('./diabetes.csv', delimiter=',')
+    print(loaded_data)
+    x_data = loaded_data[:, 0:-1]
+    t_data = loaded_data[:, [-1]]
+
+    print("x_data.shape = ", x_data.shape)
+    print("t_data.shape = ", t_data.shape)
+
+except Exception as err :
+    print(str(err))
+
+model = Sequential()
+
+model.add(Dense(t_data.shape[1], input_shape=(x_data.shape[1],), activation='sigmoid'))
+
+model.compile(optimizer=SGD(learning_rate=0.01), loss='binary_crossentropy', metrics=['accuracy'])
+
+model.summary()
